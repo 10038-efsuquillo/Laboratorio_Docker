9d3c2700824996539aba4632a4e8449024f4e5e7
Fernando Suquillo <efsuquillo@espe.edu.ec>
2023-12-19 12:04:42 -0500

    Laboratorio

---
+++ www/authentication/loginA.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<?php
+require_once(__DIR__ . '/../constants/constantes.php');
+require_once(__DIR__ . '/../DB/coneccion.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idUsuario = 1;
+    $password = $_POST["password"];
+
+    $conexion = coneccion();
+
+    $query = "SELECT * FROM veris.usuarios WHERE IdUsuario = '$idUsuario' AND Password = '$password'";
+    $result = $conexion->query($query);
+
+    if ($result->num_rows > 0) {
+        $_SESSION["idUsuario"] = $idUsuario;
+        $_SESSION["rol"] = 1;
+        header("Location: ../unido.php");
+        exit();
+    } else {
+        header("Location: loginAdmin.php?error=Credenciales incorrectas. Por favor, inténtelo de nuevo.");
+        exit();
+    }
+
+    $conexion->close();
+}
+?>
---
+++ www/authentication/loginP.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<?php
+require_once(__DIR__ . '/../constants/constantes.php');
+require_once(__DIR__ . '/../DB/coneccion.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idUsuario = $_POST["usuario"];
+    $password = $_POST["password"];
+
+    $conexion = coneccion();
+
+    $query = "SELECT * FROM veris.usuarios WHERE IdUsuario = '$idUsuario' AND Password = '$password'";
+    $query2 = "SELECT * FROM veris.pacientes WHERE IdUsuario = '$idUsuario'" ;
+    $result = $conexion->query($query);
+
+    if ($result->num_rows > 0) {
+        $_SESSION["idUsuario"] = $idUsuario;
+        $_SESSION["rol"] = 3;
+        $_SESSION["IdPaciente"] = $conexion->query($query2)->fetch_assoc()["IdPaciente"];
+        header("Location: ../unido.php");
+        exit();
+    } else {
+        header("Location: loginPacientes.php?error=Credenciales incorrectas. Por favor, inténtelo de nuevo.");
+        exit();
+    }
+
+    $conexion->close();
+}
+?>
